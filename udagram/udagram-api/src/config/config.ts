c2745e4a5ffb520a25e12c3ebd9f1b2d47a94d27
tarekelassadym <tarekelassady@gmail.com>
2023-01-30 16:00:12 +0200

    update contents

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,11 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
-// - AWS_ACCESS_KEY_ID
-// - AWS_SECRET_ACCESS_KEY
-// Are Also needed
-
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
@@ -18,6 +13,8 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
